db724241348db26783ce5d54a80970cb15646569
makeabignews <makeabignews@sina.com>
2017-10-28 01:17:17 +0800

    Update gpiopi.js

--- gpiopi.js
+++ gpiopi.js
@@ -2,19 +2,21 @@ var fs=require('fs');
 var path='/sys/class/gpio/';
 module.exports={
 	in:function(number){
-        if(!fs.existsSync(path+'gpio'+number+'/value')){
-            fs.writeFileSync(path+'export',number);
-			fs.writeFileSync(path+'gpio'+number+'/direction','in');
+        if(fs.existsSync(path+'gpio'+number+'/value')){
+            fs.writeFileSync(path+'unexport',number);
         }
+        fs.writeFileSync(path+'export',number);
+        fs.writeFileSync(path+'gpio'+number+'/direction','in');
 	    var contentText = fs.readFileSync(path+'gpio'+number+'/value','utf-8');
 		var value=parseInt(data);
 		return value;
 	},
 	out:function(number,value){
-        if(!fs.existsSync(path+'gpio'+number+'/value')){
-            fs.writeFileSync(path+'export',number);
-			fs.writeFileSync(path+'gpio'+number+'/direction','out');
+        if(fs.existsSync(path+'gpio'+number+'/value')){
+            fs.writeFileSync(path+'unexport',number);
         }
+        fs.writeFileSync(path+'export',number);
+        fs.writeFileSync(path+'gpio'+number+'/direction','out');
 		fs.writeFileSync(path+'gpio'+number+'/value',value);
 	},
 	clear:function(number){
